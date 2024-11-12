6d7a6e9f83af0d0cd54e3dec8f6157c295a53105
Ivan Levitskiy <levitskiyivan3@gmail.com>
2024-11-12 23:19:36 +0200

    add unit_sprite class, and some upgreat project
    Former-commit-id: edd1cbc5ee281d63a36ea13bc9bf008b2cf4beca

--- OOP/2D RPG/2D RPG/Unit_sprite/Unit_Sprite.h
+++ OOP/2D RPG/2D RPG/Unit_sprite/Unit_Sprite.h
@@ -1,18 +1,30 @@
 ﻿#pragma once
-#include "Animation/Animation.h" // Необхідно для використання класу Animation
+#include"Animation/Animation.h"
+#include<iostream>
 
-class Unit_Sprite {
+class Unit_Sprite 
+{
 private:
-    sf::Sprite sprite;              // Спрайт для відображення
-    sf::Texture texture;            // Текстура
-    Animation animation;            // Анімація
+	sf::Sprite unit_sprite;
+	sf::Texture unit_texture;
+    sf::Texture* currentTexture;
+	const std::string name_unit_texture;
+	sf::Vector2f position_sprite;
+
+    std::map<std::string, Animation> animations; // Набір анімацій
+    Animation* currentAnimation;                 // Поточна анімація
+    std::string idleAnimationName = "idle";
 
 public:
-    Unit_Sprite(const std::string& texture_file = "", sf::Vector2f position = {}, sf::Vector2f scale = { 1.f, 1.f });
+	Unit_Sprite(const std::string name_file = "", sf::Vector2f pos = {0,0}, bool full_photo = false, sf::IntRect Size = {0,0,0,0});
 
-    void update(float deltaTime);             // Оновлює анімацію
-    void render(sf::RenderTarget& target);   // Відображає спрайт
-    void setPosition(sf::Vector2f position); // Встановлює позицію спрайта
-    void setScale(sf::Vector2f scale);       // Встановлює масштаб спрайта
-    void addAnimationFrame(const sf::IntRect& frame); // Додає кадр до анімації
+    void setPosition(sf::Vector2f pos) { unit_sprite.setPosition(pos); };
+    void move(sf::Vector2f offset);
+    void update(float deltaTime);
+    void draw(sf::RenderWindow& window) { window.draw(unit_sprite); };
+
+    // Робота з анімаціями
+    void startAnimation(const std::string& name);
+    void handleEvent(const sf::Event& event); // Обробка подій
+
 };
